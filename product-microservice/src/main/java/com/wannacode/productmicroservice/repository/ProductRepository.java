27e2117fc63dc8dfdd2ce336f129cb1d2e06375e
Johansel <johansel.calderon@optimissa.com>
2022-12-19 16:58:03 +0100

    actualizado

--- product-microservice/src/main/java/com/wannacode/productmicroservice/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.wannacode.productmicroservice.repository;
-
-import com.wannacode.productmicroservice.entity.ProductEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
-}
