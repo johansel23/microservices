255b92f19385774ebad26e683605b049c29ed230
Johansel <johansel.calderon@optimissa.com>
2022-12-22 16:07:08 +0100

    Agregado en el FeignCliente el Path = "/car" y Path = "/bike" ya que sin esto no se conecta al @GetMappit de cada micro servicio. Todo esta funcionando correctamente hasta este punto

--- user-service/src/main/java/com/tutorial/userservice/feignClients/BikeFeignClient.java
+++ user-service/src/main/java/com/tutorial/userservice/feignClients/BikeFeignClient.java
@@ -2,14 +2,11 @@ package com.tutorial.userservice.feignClients;
 
 import com.tutorial.userservice.model.Bike;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "bike-service")
+@FeignClient(name = "bike-service", path = "/bike")
 public interface BikeFeignClient {
 
 
--- user-service/src/main/java/com/tutorial/userservice/feignClients/CarFeignClient.java
+++ user-service/src/main/java/com/tutorial/userservice/feignClients/CarFeignClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name = "car-service")
+@FeignClient(name = "car-service", path = "/car")
 public interface CarFeignClient {
 
     @PostMapping()
